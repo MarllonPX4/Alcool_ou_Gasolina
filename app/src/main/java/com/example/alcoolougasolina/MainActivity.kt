894fae7a250bce74cd36d120e9234e5f68c24e82
MarllonPX4 <marllonprogramming@gmail.com>
2024-02-05 07:34:26 -0300

    Validation

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editAlcool: TextInputEditText
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editGasolina: TextInputEditText
+
+    private  lateinit var buttonCalcular: Button
+    private  lateinit var textResult: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inicializarComponentesInterface()
+        buttonCalcular.setOnClickListener {
+            calcularMelhorPreco()
+        }
+    }
+
+    private fun calcularMelhorPreco() {
+        val precoAlcool = editAlcool.text.toString()
+        val precoGasolina = editGasolina.text.toString()
+
+        val validacao = validarCampos(precoAlcool, precoGasolina)
+
+        if(validacao){
+            val resultado = precoAlcool.toDouble() / precoGasolina.toDouble()
+
+            if (resultado >= 0.7){
+                textResult.text = "Melhor utilizar Gasolina"
+            }else{
+                textResult.text = "Melhor utilizar Álcool"
+            }
+        }
+    }
+
+    private fun validarCampos(pAlcool: String, pGasolina: String): Boolean {
+
+        textInputAlcool.error = null
+        textInputGasolina.error = null
+
+        if(pAlcool.isEmpty()){
+            textInputAlcool.error = "Digite o preço do Álcool"
+            return false
+        }else if (pGasolina.isEmpty()){
+            textInputGasolina.error = "Digite o preço da Gasolina"
+            return false
+        }
+        return true
+    }
+
+    private fun inicializarComponentesInterface(){
+        textInputAlcool = findViewById(R.id.text_Input_Alcool)
+        editAlcool = findViewById(R.id.edit_Alcool)
+        textInputGasolina = findViewById(R.id.text_Input_Gasolina)
+        editGasolina = findViewById(R.id.edit_Gasolina)
+
+        buttonCalcular = findViewById(R.id.button_Calcular)
+        textResult = findViewById(R.id.text_Result)
     }
 }
